bea041a3c05b3461edab80eeca59084387fe4fb9
Daniel Gather <163533297+DanielGather@users.noreply.github.com>
2026-01-31 15:46:23 +0100

    added a tab menu for differents states

--- src/app/arbeitsschutz/arbeitsschutz.ts
+++ src/app/arbeitsschutz/arbeitsschutz.ts
@@ -2,6 +2,9 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { RssService, RssItem } from '../service/rss';
+import { signal } from '@angular/core';
+
+type TabKey = 'rss' | 'news' | 'settings';
 
 @Component({
   standalone: true,
@@ -13,4 +16,10 @@ export class Arbeitsschutz {
   private rss = inject(RssService);
 
   readonly items = toSignal(this.rss.loadBaua(), { initialValue: [] as RssItem[] });
+
+  activeTab = signal<TabKey>('rss');
+
+  setTab(tab: TabKey) {
+    this.activeTab.set(tab);
+  }
 }
